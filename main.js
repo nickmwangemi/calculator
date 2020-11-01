0aeef70f13d4576e41c74379333745411db52baa
nickmwangemi <nickmwangemi@gmail.com>
2020-11-01 13:40:10 +0300

    Add clear functionality

--- main.js
+++ main.js
@@ -7,20 +7,20 @@ let hasSign
 
 // Target UI elements
 // Display
-let working = document.getElementById('#top-display')
-let answer = document.getElementById('#answer-display')
+let working = document.getElementById('top-display')
+let answer = document.getElementById('answer-display')
 // Function buttons
-let equal = document.getElementById('#equal-sign')
-let cancel = document.getElementById('#clear')
-let backspace = document.getElementById('#delete')
-let decimal = document.getElementById('#decimal')
-let sign = document.getElementById('#sign')
+let equal = document.getElementById('equal-sign')
+let clearButton = document.getElementById('clear')
+let backspace = document.getElementById('delete')
+let decimal = document.getElementById('decimal')
+let sign = document.getElementById('sign')
 
 // Operators
-let add = document.getElementById('#add')
-let minus = document.getElementById('#minus')
-let multiply = document.getElementById('#multiply')
-let divide = document.getElementById('#divide')
+let add = document.getElementById('add')
+let minus = document.getElementById('minus')
+let multiply = document.getElementById('multiply')
+let divide = document.getElementById('divide')
 
 // Display numbers
 let numbers = document.querySelectorAll('#calculator .number')
@@ -29,4 +29,14 @@ numbers.forEach(function (number) {
 		working.textContent += number.innerHTML
 		currentNumber += number.innerHTML
 	})
+})
+
+// Clear
+clearButton.addEventListener('click', function () {
+	working.textContent = ''
+	answer.textContent = ''
+	currentNumber = ''
+	firstNumber = undefined
+	secondNumber = undefined
+	operator = undefined
 })
